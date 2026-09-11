016f623550c931c327a232f2b12199b4bb0046f1
Pedro Mendes <pedrohrmendes@proton.me>
2026-09-11 13:07:10 -0300

    fix(chart): name the duckdb secret gcs

--- helm/files/sql/create_s3_secret.sql
+++ helm/files/sql/create_s3_secret.sql
@@ -1,6 +1,6 @@
 SELECT duckdb.raw_query(
     format(
-        'CREATE OR REPLACE PERSISTENT SECRET s3_gcs (TYPE s3, KEY_ID %L, SECRET %L, REGION ''us-east-1'', ENDPOINT %L, URL_STYLE ''path'', USE_SSL %s)',
+        'CREATE OR REPLACE PERSISTENT SECRET gcs (TYPE s3, KEY_ID %L, SECRET %L, REGION ''us-east-1'', ENDPOINT %L, URL_STYLE ''path'', USE_SSL %s)',
         :'gcs_key_id',
         :'gcs_secret_key',
         :'gcs_endpoint',
